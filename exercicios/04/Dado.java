74b7e0a02ee76a4fee10de665aab718fbfb8bfa6
Lucas Lopes Pereira <44032349+LucasPLopes@users.noreply.github.com>
2019-05-14 18:07:50 -0300

    Update Dado.java

--- exercicios/04/Dado.java
+++ exercicios/04/Dado.java
@@ -9,12 +9,12 @@ public class Dado{
 			SecureRandom sr = new SecureRandom();
 			int [] frequencia = new int[13];
 			
-			for(int i= 0 ; i < 1000000 ; i++	)
+			for(int i= 0 ; i < 1000000 ; i++)
 			++frequencia[1+ sr.nextInt(12)];
 			
 			
-			System.out.println("Quantas vezes um lado irá se repetir ao lançarmos um dado 1000000 vezes");
-			System.out.println("Face x Quantidade");
+			System.out.println("Qual a soma se lançarmos dois dados.");
+			System.out.println("Soma x Quantidade:");
 			for( int i =1 ; i<= 12;i++)
 				System.out.printf("%d\t%d\n",i, frequencia[i]);
 		

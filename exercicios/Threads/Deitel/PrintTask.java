a82b797797aabb292adf33d2323a3ca994714e80
Lucas Lopes Pereira <44032349+LucasPLopes@users.noreply.github.com>
2019-05-20 17:15:11 -0300

    Create PrintTask.java
    Exemplo 01 do livro dos Deitel com modificações para introdução a programação concorrente.

---
+++ exercicios/Threads/Deitel/PrintTask.java
@@ -0,0 +1,34 @@
+import java.security.SecureRandom;
+
+
+public class PrintTask implements Runnable{
+	private static final SecureRandom generator = new SecureRandom();
+	private final int sleepTime;
+	private final String Nome;
+	
+	
+	public PrintTask( String pt){
+		Nome = pt;
+		sleepTime = generator.nextInt(7000);
+	}
+	
+	public void run(){
+		try{
+			System.out.printf("%s irá dormir por %d milisegundos.%n",Nome, sleepTime);
+			Thread.sleep(sleepTime);
+			
+		}catch (InterruptedException e){
+			e.printStackTrace();
+			Thread.currentThread().interrupt();
+			
+			
+		}
+		System.out.printf("%s terminou a soneca%n",Nome);
+	}
+	
+	
+	public static void main(String [] args){
+			PrintTask pt = new PrintTask("Minha Thread");
+			pt.run();			
+	}	
+}

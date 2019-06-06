797b610f3e3ad608967435c1acc8e21d0ac1365b
Lucas Lopes Pereira <44032349+LucasPLopes@users.noreply.github.com>
2019-06-06 15:26:54 -0300

    Create A.java

---
+++ exercicios/UML/pacote1/A.java
@@ -0,0 +1,25 @@
+package pacote1;
+
+class A {
+    public int at1;
+    private String at2 = "";
+    protected float at3 = 7f;
+    String at4 = "";
+
+    public void m0() {
+        System.out.println("M0()");
+    }
+
+    float m3() {
+        return 3f;
+    }
+
+    float m4() {
+        return 4f;
+    }
+
+    static String m5() {
+        return "M5()";
+    }
+
+}

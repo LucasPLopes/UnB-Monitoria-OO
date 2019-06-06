ab713db4768dbb30e6ee431205d7d7b595461d14
Lucas Lopes Pereira <44032349+LucasPLopes@users.noreply.github.com>
2019-06-06 15:28:06 -0300

    Create D.java

---
+++ exercicios/UML/pacote2/D.java
@@ -0,0 +1,25 @@
+package pacote2;
+import pacote1.C;
+
+public class D extends C{
+    public char at17 = 'D';
+    protected float at18 = 18f;
+    static String  at19 = "at19";
+    private int at20 = 20;
+
+    void m6(){
+        System.out.println("M6()");
+    }
+    private char m7(){
+        return at17;
+    }
+
+    public String m8(){
+        return D.at19;
+    }
+
+    public static void main(String[] args) {
+        
+    }
+
+}

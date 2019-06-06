e0adb039f5836d3fe06ae87d08292c539cba6e42
Lucas Lopes Pereira <44032349+LucasPLopes@users.noreply.github.com>
2019-06-06 15:28:26 -0300

    Create E.java

---
+++ exercicios/UML/pacote2/E.java
@@ -0,0 +1,23 @@
+package pacote2;
+
+class E{
+    public boolean at21 = false;
+    static protected char at22 = 'E';
+    protected String at23 = "at23";
+
+    D d ;
+
+    public  E(){
+        d = new D();
+    }
+
+    protected void m2()
+    {
+        System.out.println("M2()");
+    }
+
+    static float m10(){
+        return 10f;
+    }
+
+}

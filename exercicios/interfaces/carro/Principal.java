6f108cb9bbd6a645a28eb48638f4f6d894e2d9ec
lucas <l.lopes.fga@gmail.com>
2019-07-17 12:36:44 -0300

    Classe main

---
+++ exercicios/interfaces/carro/Principal.java
@@ -0,0 +1,13 @@
+public class Principal{
+    public static void main(String[] args) {
+        Carro puma = new Carro(155,50);
+
+        puma.exibirOdometro();
+        puma.acelerar();
+        puma.ativarTurbo();
+        puma.ativarTurbo();
+        puma.ativarTurbo();
+        
+        
+    }
+}

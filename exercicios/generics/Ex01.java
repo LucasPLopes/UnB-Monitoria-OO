59e561cf3c1e70c42722a35ea45ccf2eae8d9e05
lucas <l.lopes.fga@gmail.com>
2019-07-22 15:21:27 -0300

    Adicionado exemplo com Inner Class

--- exercicios/generics/Ex01.java
+++ exercicios/generics/Ex01.java
@@ -1,9 +1,24 @@
 import java.util.ArrayList;
 
-public class Ex01{
+public class Ex01 {
+
+    private class Animal {
+        String nome;
+
+        public Animal(String nome) {
+            this.nome = nome;
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + "Animal [nome=" + nome + "]";
+        }
+
+    }
+
     public static void main(String[] args) {
         {
-            /**Antes do Java 5.0 **/
+            /** Antes do Java 5 **/
             ArrayList frutas = new ArrayList();
 
             frutas.add("banana");
@@ -11,23 +26,36 @@ public class Ex01{
             frutas.add("uva");
             frutas.add("maça");
             System.out.println("-----Sem generics-----\n");
-            System.out.println("Sem casting: "+frutas.get(3));// warnning: operacoes inseguras
+            System.out.println("Sem casting: " + frutas.get(3));// warnning: operacoes inseguras
             String fruta = (String) frutas.get(3);
-            System.out.println("Com casting: "+fruta);
+            System.out.println("Com casting: " + fruta);
 
             System.out.println("\n\n-----Usando generics-----\n");
-         }
+        }
 
-         {
-             ArrayList<String> animais = new ArrayList<String>();
+        {
+            ArrayList<String> animais = new ArrayList<String>();
 
             animais.add("Cachorro");
             animais.add("Gato");
             animais.add("Papagaio");
             animais.add("Porquinho-da-índia");
-            
+
             String animal = animais.get(3);
             System.out.println(animal);
-         }
+        }
+        
+        {
+
+            /** Estilo de Array a partir do java 7 **/
+            System.out.println("\n\n-----A partir do Java 7 mais Inner Class-----\n");
+            ArrayList<Ex01.Animal> animais = new ArrayList<>();
+            animais.add(new Ex01().new Animal("Cachorro"));
+            animais.add(new Ex01().new Animal("Papagaio"));
+            animais.add(new Ex01().new Animal("Gato"));
+            animais.add(new Ex01().new Animal("Porquinho-da-índia"));
+
+            System.out.println(animais.get(3).toString());
+        }
     }
 }
